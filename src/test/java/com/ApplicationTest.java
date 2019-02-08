8f812c4c9db301becec3eac6e601d8ed7e7ce731
nileshdevdas <nilesh.devdas@gmail.com>
2019-02-08 17:58:57 +0530

    Initial Commit

---
+++ src/test/java/com/ApplicationTest.java
@@ -0,0 +1,22 @@
+package com;
+
+import static org.junit.Assert.*;
+
+public class ApplicationTest {
+
+    @org.junit.Test
+    public void getPassword() {
+    }
+
+    @org.junit.Test
+    public void getUsername() {
+    }
+
+    @org.junit.Test
+    public void setPassword() {
+    }
+
+    @org.junit.Test
+    public void setUsername() {
+    }
+}
